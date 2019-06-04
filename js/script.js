3e25376a412f45d57468333f6d5bf16002467956
roshanbandara <47655552+roshanbandara@users.noreply.github.com>
2019-06-04 07:24:21 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ var accHD = document.getElementsByClassName('accordionItemHeading');
         }
     }
 
+
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function() {scrollFunction()};
 
@@ -20,8 +21,27 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "10px 10px";
     document.getElementById("logo").style.fontSize = "20px";
+    // document.getElementById("logo").style.display = "none";
   } else {
     document.getElementById("navbar").style.padding = "15px 10px";
     document.getElementById("logo").style.fontSize = "35px";
+    // document.getElementById("logo").style.display = "block";
+    // document.getElementById("navbar").style.transition = "transform 0.4s ease";
   }
 }
+
+
+
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    // document.body.style.backgroundColor = "yellow";
+    document.getElementById("logo").style.display = "none";
+  } else {
+    document.getElementById("logo").style.display = "block";
+  }
+}
+
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
